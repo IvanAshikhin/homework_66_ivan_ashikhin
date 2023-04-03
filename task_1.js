dc8c4bfbb5d0f8780b4bfdf29f83183dbd156be0
Ivan <ashikhin1998@gmail.com>
2023-04-03 23:12:18 +0600

    Выполнил первое задание

---
+++ task_1.js
@@ -0,0 +1,13 @@
+let string1 = document.getElementById('string-1');
+let string2 = document.getElementById('string-2');
+let string3 = document.getElementById('string-3')
+let string4 = document.getElementById('string-4');
+let string5 = document.getElementById('string-5');
+let string6 = document.getElementById('string-6');
+
+console.log(string3.textContent)
+console.log(string5.textContent)
+console.log(string2.textContent)
+console.log(string6.textContent)
+console.log(string4.textContent)
+console.log(string1.textContent)
