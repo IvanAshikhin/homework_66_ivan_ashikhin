516c960f2cb92131122d43e24b0c020ad7c292d7
Ivan <ashikhin1998@gmail.com>
2023-04-03 23:19:22 +0600

    Выполнил второе задание

---
+++ task_2.js
@@ -0,0 +1,7 @@
+let elements = document.getElementsByClassName('element');
+for (let i = 0; i < 3; i++) {
+    elements[i].style.color = 'red';
+}
+for (let i = 3; i < 6; i++) {
+    elements[i].style.color = 'green';
+}
