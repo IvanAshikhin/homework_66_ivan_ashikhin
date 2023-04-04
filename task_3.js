75049558e154396a50d80ee777cd63d7c9ccf44f
Ivan <ashikhin1998@gmail.com>
2023-04-04 16:28:09 +0600

    Сделал каждую строку рандомного цвета

--- task_3.js
+++ task_3.js
@@ -1,8 +1,12 @@
 let container = document.getElementById('container')
-
 for (let i = 1; i < 6; i++) {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    let color = "#" + red.toString(16) + green.toString(16) + blue.toString(16);
+
     let div = document.createElement('div');
     div.className = "element";
-    div.innerHTML = `Element ${i}`;
+    div.innerHTML = `<p style="color: ${color}">Element ${i} </p>`;
     container.appendChild(div)
 }
